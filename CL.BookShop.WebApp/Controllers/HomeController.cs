45fc932926e1c06b57481bc8f891aa51a892482c
taohonggou <cl889521@163.com>
2015-08-16 07:40:04 +0800

    不能添加Common引用

---
+++ CL.BookShop.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CL.BookShop.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //public ActionResult GetVerificCode()
+        //{
+        //    validate
+        //}
+    }
+}
